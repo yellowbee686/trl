5513f06fbeb5da028994b6906b135a22621d6f6c
ChengXiao686 <xiao.cheng@autra.tech>
2024-03-19 16:21:40 +0800

    add sh

---
+++ examples/scripts/reward_modeling.sh
@@ -0,0 +1,14 @@
+python examples/scripts/reward_modeling.py \
+    --model_name_or_path=facebook/opt-350m \
+    --output_dir="reward_modeling_anthropic_hh" \
+    --per_device_train_batch_size=64 \
+    --num_train_epochs=1 \
+    --gradient_accumulation_steps=16 \
+    --gradient_checkpointing=True \
+    --learning_rate=1.41e-5 \
+    --report_to="wandb" \
+    --remove_unused_columns=False \
+    --optim="adamw_torch" \
+    --logging_steps=10 \
+    --evaluation_strategy="steps" \
+    --max_length=512 \
