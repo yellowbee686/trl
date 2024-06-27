281c5c7f813d9f69619adc140fd66d23ec5ec343
ChengXiao686 <xiao.cheng@autra.tech>
2024-06-27 20:51:26 +0800

    add sft

--- examples/scripts/download.py
+++ examples/scripts/download.py
@@ -1,9 +1,12 @@
 from datasets import load_dataset
 # Load model directly
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSequenceClassification
 
 tokenizer = AutoTokenizer.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
 model = AutoModelForSequenceClassification.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
+
+tokenizer = AutoTokenizer.from_pretrained("RLHFlow/LLaMA3-SFT")
+model = AutoModelForCausalLM.from_pretrained("RLHFlow/LLaMA3-SFT")
 
 ds = load_dataset("BAAI/Infinity-Instruct")
 
