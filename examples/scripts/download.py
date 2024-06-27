5be6fc29e8ce2a24a24ff802b0efcc8c9186cc00
ChengXiao686 <xiao.cheng@autra.tech>
2024-06-27 20:47:04 +0800

    add reward model

--- examples/scripts/download.py
+++ examples/scripts/download.py
@@ -1,3 +1,9 @@
 from datasets import load_dataset
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
+model = AutoModelForSequenceClassification.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
 
 ds = load_dataset("BAAI/Infinity-Instruct")
+
