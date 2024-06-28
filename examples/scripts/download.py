d6507846cc1f73b43be5ea3b7839fa001da0f512
ChengXiao686 <xiao.cheng@autra.tech>
2024-06-28 11:34:39 +0800

    add BT reward

--- examples/scripts/download.py
+++ examples/scripts/download.py
@@ -2,11 +2,13 @@ from datasets import load_dataset
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSequenceClassification
 
-tokenizer = AutoTokenizer.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
-model = AutoModelForSequenceClassification.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
+# tokenizer = AutoTokenizer.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
+# model = AutoModelForSequenceClassification.from_pretrained("RLHFlow/ArmoRM-Llama3-8B-v0.1", trust_remote_code=True)
 
-tokenizer = AutoTokenizer.from_pretrained("RLHFlow/LLaMA3-SFT")
-model = AutoModelForCausalLM.from_pretrained("RLHFlow/LLaMA3-SFT")
+# tokenizer = AutoTokenizer.from_pretrained("RLHFlow/LLaMA3-SFT")
+# model = AutoModelForCausalLM.from_pretrained("RLHFlow/LLaMA3-SFT")
 
-ds = load_dataset("BAAI/Infinity-Instruct")
+# ds = load_dataset("BAAI/Infinity-Instruct")
 
+tokenizer = AutoTokenizer.from_pretrained("sfairXC/FsfairX-LLaMA3-RM-v0.1")
+model = AutoModelForSequenceClassification.from_pretrained("sfairXC/FsfairX-LLaMA3-RM-v0.1")
